7b1052f1a9bc1d523e4424e5be6d5b35444a7299
garbich <131876643+garbich@users.noreply.github.com>
2024-01-23 22:50:20 +0200

    Add files via upload

---
+++ 20.12.2023/Task 1/Task 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MySpace
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string email = "thug@itstep.ua";
+
+            var result = Regex.Match(email, "^[a-zA-Z0-9]+[@]+(itstep.ua)+$");
+
+            bool a = result.Success;
+
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ 20.12.2023/Task 2/Task 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+namespace MySpace
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string number = "+380239870167";
+
+            var result = Regex.Match(number, @"^\+380\d{9}$");
+
+            bool a = result.Success;
+
+            Console.WriteLine(a);
+        }
+    }
+}
